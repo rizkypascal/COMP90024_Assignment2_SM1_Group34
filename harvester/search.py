0ef8e7b66b74f9c51394dfc43de663b998a50471
Rizky Paskalis <rizky.paskalis@wartek,belajar.id>
2022-04-23 23:39:32 +1000

    add dockerise and exception handler

--- harvester/search.py
+++ harvester/search.py
@@ -1,21 +1,49 @@
+from twitter_utils import TwitterUtils
+import tweepy
+import couchdb
+import logging
+import logger
+
 """
     TODO:
         - error handling duplicate entry on tweet
         - error handling when reach limit
         - filtering with location, maxId / sinceId
 """
+
+tweets_count = 0 # to count how many tweets have been saved
+
 class Search:
     def __init__(self, api, db, query):
         self.api = api
         self.db = db
         self.query = query
+        self.tweets_count = tweets_count
     
     def save(self, tweet):
         self.db.save(tweet._json)
     
     def search(self):
-        for tweet in self.api.search_tweets(q=self.query, count=1):
-            self.save(tweet)
+        try:
+            for tweet in self.api.search_tweets(q=self.query, count=1):
+                try:
+                    self.save(tweet)
+                    self.tweets_count += self.tweets_count + 1
+                except (couchdb.http.Unauthorized, couchdb.http.ResourceNotFound) as e:
+                    logging.error("Couch db resource error: {}".format(str(e)))
+                    break
+                except (BaseException) as e:
+                    logging.warning("Couch db save: {}".format(str(e)))
+                    continue
+        except tweepy.errors.TweepyException as e:
+            logging.error("Twitter search error: {}".format(str(e)))
 
-
-
+if __name__ == "__main__":
+    twitter_utils = TwitterUtils()
+    api = twitter_utils.client()
+    db = twitter_utils.db()
+    
+    while True:
+        logging.info("harvesting search starting")
+        search = Search(api, db, "test")
+        search.search()
--- harvester/stream.py
+++ harvester/stream.py
@@ -1,4 +1,7 @@
+from twitter_utils import TwitterUtils
 import tweepy
+import logging
+import logger
 
 """
     TODO:
@@ -7,6 +10,12 @@ import tweepy
         - filtering with location
         - define bounding box melbourne
 """
+
+API_KEY = "j95ClC3IehZBDJQNMRTW9gQGl"
+API_SECRET = "uymxiL0nOkD5kNTT5PRCYNyj0eWIprpEYoG73nNTJ8KHDydCnF"
+ACCESS_TOKEN = "1496071983326703617-uUvH0f50872h4djpu7KAF2HXzxra1Y"
+ACCESS_TOKEN_SECRET = "82GngChsZKfYwFkNa8aqaMI4qv6KJoA0nq76Px2aKejac"
+
 class StreamListener(tweepy.Stream):
     def __init__(self, *args, **kwargs):
         super(StreamListener, self).__init__(*args)
@@ -27,4 +36,14 @@ class Stream:
         self.db = db
     def stream(self):
         stream = StreamListener(self.apiKey, self.apiSecret, self.accessToken, self.accessTokenSecret, db = self.db)
-        stream.filter(track=["Melbourne"])#,locations=[144.931641,-37.814124,144.931641,-37.814124])
+        stream.filter(locations=[143.415527,-38.796908,146.260986,-36.553775]) #Melbourne and surrounding (outer area is Maryborough, Bendigo, Seymour, Ballarat, Colac, Rosebud, Warragul)
+
+if __name__ == "__main__":
+    twitter_utils = TwitterUtils()
+    api = twitter_utils.client()
+    db = twitter_utils.db()
+    
+    while True:
+        logging.info("harvesting stream starting")
+        stream = Stream(API_KEY, API_SECRET,ACCESS_TOKEN ,ACCESS_TOKEN_SECRET, db)
+        stream.stream()
