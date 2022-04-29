f206da0a673965329011a3aae36e717a7e6d71d8
Georgia Lewis <grlewis@student.unimelb.edu.au>
2022-04-29 14:49:56 +1000

    created basic frontend and backend for app

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+
+function App() {
+
+  const [data, setData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("/members").then(
+      res => res.json()
+    ).then(
+      data => {
+        setData(data)
+        console.log(data)
+      }
+    )
+  }, [])
+  return (
+    // Checking if members array is equal to undefined or not - if so, it means APi is being fetched
+    // Otherwise it has been fetched and will display members one by one
+    <div>
+
+
+      {(typeof data.members === "undefined") ? (
+        <p>Loading...</p>
+      ) : (
+        data.members.map((member, i) => (
+          <p key={i}>{member}</p>
+        ))
+      )}
+
+    </div>
+  )
+}
+
+export default App
