76edde0475eb4bc44ae916ce468f6528c89d5eec
Juny Kesumadewi <juny@ravpn-266-2-student-ten-8-71-171.uniaccess.unimelb.edu.au>
2022-05-01 16:13:46 +1000

    Handle multiPolygons

--- analysis/assign_lga.py
+++ analysis/assign_lga.py
@@ -18,34 +18,48 @@ def load_polygons():
             "geometry": {
                 "$ne": None
             }
-        }
+        },
+        "limit": 100
     }
 
     all_polygons = []
     for doc in db.find(query):
         try:
-            coordinates = doc.get("geometry", {}).get("coordinates", [])[0]
+            # One of: "MultiPolygon", "Polygon"
+            geo_type = doc.get("geometry", {}).get("type")
 
-            lons_vect = []
-            lats_vect = []
-            for point in coordinates:
-                lons_vect.append(point[0])
-                lats_vect.append(point[1])
+            all_rows = doc.get("geometry", {}).get("coordinates", [])
+            # if not multi, make it an array
+            if geo_type == "Polygon":
+                all_rows = [all_rows]
 
-            lons_lats_vect = np.column_stack((lons_vect, lats_vect)) # Reshape coordinates
-            polygon = Polygon(lons_lats_vect) # create polygon
-            polygon = Polygon(coordinates) # create polygon
+            for row in all_rows:
+                coordinates = doc.get("geometry", {}).get("coordinates", [])[0]
 
-            obj = {
-                "data": {
-                    "lga_name_2016": doc.get("properties", {}).get("lga_name_2016"),
-                    "lga_code_2016": doc.get("properties", {}).get("lga_code_2016"),
-                },
-                "polygon": polygon
-            }
-            all_polygons.append(obj)
+                if geo_type == "MultiPolygon":
+                    coordinates = coordinates[0]
+
+                lons_vect = []
+                lats_vect = []
+                for point in coordinates:
+                    lons_vect.append(point[0])
+                    lats_vect.append(point[1])
+
+                lons_lats_vect = np.column_stack((lons_vect, lats_vect)) # Reshape coordinates
+                polygon = Polygon(lons_lats_vect) # create polygon
+                # polygon = Polygon(coordinates) # create polygon
+
+                obj = {
+                    "data": {
+                        "lga_name_2016": doc.get("properties", {}).get("lga_name_2016"),
+                        "lga_code_2016": doc.get("properties", {}).get("lga_code_2016"),
+                    },
+                    "polygon": polygon
+                }
+                all_polygons.append(obj)
         except Exception as e:
-            print("Failed to load polygon")
+            name = doc.get("properties", {}).get("lga_name_2016")
+            print(f"Failed to load polygon {name}: {e}")
 
     return all_polygons
 
